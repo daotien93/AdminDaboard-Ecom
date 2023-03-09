4922dc745556c15afd03a3aee41d230b171c34c0
Henrry Tien (Dao Tien) <58262966+DTien93@users.noreply.github.com>
2023-03-09 22:47:04 +0700

    client

--- src/pages/Admin/Users/Users.tsx
+++ src/pages/Admin/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect,useState } from 'react';
+import React, { ChangeEvent, Fragment, useEffect,useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { AppState } from '../../../store';
@@ -11,6 +11,8 @@ export const Users = () => {
   const totalItems = useSelector((state: AppState) => state.users.total);
   const pageSize = useSelector((state: AppState) => state.users.pageSize);
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -20,6 +22,15 @@ export const Users = () => {
   const onPageChanged = (pageNumber: number) => {
     setCurrentPage(pageNumber);
     dispatch(loadUsersPaging(pageNumber));
+  }
+
+  const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value);
+  }
+
+  const enableSearch = () => {
+    setSearchKeyword('');
+    dispatch(loadUsersPaging('', 1));
   }
 
   const userElements: JSX.Element[] = users.map((user) => {
@@ -36,8 +47,55 @@ export const Users = () => {
     <Fragment>
       <div>
         <h1 className='h3 mb-2 text-gray-800'>Danh sách người dùng</h1>
+        {showSearch && (
+          <div className='row mb-3'>
+            <div className='col-xl-12 col-md-12 mb-12'>
+              <div className='card'>
+                <h5 className='card-header'>Tìm kiếm</h5>
+                <div className='header-buttons'>
+                  <button
+                    className='btn btn-default'
+                    onClick={() => setShowSearch(false)}
+                  >
+                    Đóng
+                    <i className='fas fa-times'></i>
+                  </button>
+                </div>
+                <div className='card-body'>
+                  <form className='form-inline'>
+                    <div className='col-auto'>
+                      <input
+                        type='text'
+                        value={searchKeyword}
+                        onChange={handleChangeSearch}
+                        className='form-control'
+                        placeholder='Từ khoá'
+                      />
+                    </div>
 
-        {/* DataTales Example */}
+                    <button
+                      type='button'
+                      onClick={() =>
+                        dispatch(loadUsersPaging(searchKeyword, currentPage))
+                      }
+                      className='btn btn-primary my-1'
+                    >
+                      Tìm kiếm
+                    </button>
+                    <button
+                      type='button'
+                      onClick={() => enableSearch()}
+                      className='btn btn-default my-1'
+                    >
+                      Xoá
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {/* Lists Users*/}
         <div className='card shadow mb-4'>
           <div className='card-header py-3'>
             <h6 className='m-0 font-weight-bold text-primary'>
