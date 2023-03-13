3cda6d1f376a6a4f438c5f0918299b6fb83cee0a
Henrry Tien (Dao Tien) <58262966+DTien93@users.noreply.github.com>
2023-03-13 22:09:19 +0700

    client

--- src/pages/Admin/Users/Users.tsx
+++ src/pages/Admin/Users/Users.tsx
@@ -12,26 +12,26 @@ export const Users = () => {
   const pageSize = useSelector((state: AppState) => state.users.pageSize);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchKeyword, setSearchKeyword] = useState('');
-  const [showSearch, setShowSearch] = useState(false);
+  const [showSearch, setShowSearch] = useState(true);
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(loadUsersPaging(currentPage));
-  }, [dispatch, currentPage]);
+    dispatch(loadUsersPaging(searchKeyword, currentPage));
+  }, [dispatch, currentPage, searchKeyword]);
 
   const onPageChanged = (pageNumber: number) => {
     setCurrentPage(pageNumber);
-    dispatch(loadUsersPaging(pageNumber));
-  }
+    dispatch(loadUsersPaging(searchKeyword, pageNumber));
+  };
 
   const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchKeyword(e.target.value);
-  }
+  };
 
   const enableSearch = () => {
     setSearchKeyword('');
     dispatch(loadUsersPaging('', 1));
-  }
+  };
 
   const userElements: JSX.Element[] = users.map((user) => {
     return (
@@ -45,92 +45,101 @@ export const Users = () => {
 
   return (
     <Fragment>
-      <div>
-        <h1 className='h3 mb-2 text-gray-800'>Danh sách người dùng</h1>
-        {showSearch && (
-          <div className='row mb-3'>
-            <div className='col-xl-12 col-md-12 mb-12'>
-              <div className='card'>
-                <h5 className='card-header'>Tìm kiếm</h5>
-                <div className='header-buttons'>
-                  <button
-                    className='btn btn-default'
-                    onClick={() => setShowSearch(false)}
-                  >
-                    Đóng
-                    <i className='fas fa-times'></i>
-                  </button>
-                </div>
-                <div className='card-body'>
-                  <form className='form-inline'>
-                    <div className='col-auto'>
-                      <input
-                        type='text'
-                        value={searchKeyword}
-                        onChange={handleChangeSearch}
-                        className='form-control'
-                        placeholder='Từ khoá'
-                      />
-                    </div>
+    <div>
+      <h1 className='h3 mb-2 text-gray-800'>Danh sách người dùng</h1>
+      {showSearch && (
+        <div className='row mb-3'>
+          <div className='col-xl-12 col-md-12 mb-12'>
+            <div className='card'>
+              <h5 className='card-header'>Tìm kiếm</h5>
+              <div className='header-buttons'>
+                <button
+                  className='btn btn-default'
+                  onClick={() => setShowSearch(false)}
+                >
+                  Đóng
+                  <i className='fas fa-times'></i>
+                </button>
+              </div>
+              <div className='card-body'>
+                <form className='form-inline'>
+                  <div className='col-auto'>
+                    <input
+                      type='text'
+                      value={searchKeyword}
+                      onChange={handleChangeSearch}
+                      className='form-control'
+                      placeholder='Từ khoá'
+                    />
+                  </div>
 
-                    <button
-                      type='button'
-                      onClick={() =>
-                        dispatch(loadUsersPaging(searchKeyword, currentPage))
-                      }
-                      className='btn btn-primary my-1'
-                    >
-                      Tìm kiếm
-                    </button>
-                    <button
-                      type='button'
-                      onClick={() => enableSearch()}
-                      className='btn btn-default my-1'
-                    >
-                      Xoá
-                    </button>
-                  </form>
-                </div>
+                  <button
+                    type='button'
+                    onClick={() =>
+                      dispatch(loadUsersPaging(searchKeyword, currentPage))
+                    }
+                    className='btn btn-primary my-1'
+                  >
+                    Tìm kiếm
+                  </button>
+                  <button
+                    type='button'
+                    onClick={() => enableSearch()}
+                    className='btn btn-default my-1'
+                  >
+                    Xoá
+                  </button>
+                </form>
               </div>
             </div>
           </div>
-        )}
-        {/* Lists Users*/}
-        <div className='card shadow mb-4'>
-          <div className='card-header py-3'>
-            <h6 className='m-0 font-weight-bold text-primary'>
-              Danh sách người dùng
-            </h6>
-          </div>
-          <div className='card-body'>
-            <div className='table-responsive'>
-              <table
-                className='table table-bordered'
-                id='dataTable'
-                width='100%'
-                cellSpacing={0}
-              >
-                <thead>
-                  <tr>
-                    <th>Tên</th>
-                    <th>Họ</th>
-                    <th>Email</th>
-                  </tr>
-                </thead>
-                <tbody>{userElements}</tbody>
-              </table>
-            </div>
-          </div>
-          <div className='card-footer'>
-            <Pagination
-              totalRecords={totalItems}
-              pageLimit={5}
-              pageSize={pageSize}
-              onPageChanged={onPageChanged}
-            ></Pagination>
+        </div>
+      )}
+      {/* DataTales Example */}
+      <div className='card shadow mb-4'>
+        <div className='card-header py-3'>
+          <h6 className='m-0 font-weight-bold text-primary'>
+            Danh sách người dùng
+          </h6>
+        </div>
+        <div className='header-buttons'>
+          <button
+            type='button'
+            className='btn btn-link'
+            onClick={() => setShowSearch(true)}
+          >
+            Tìm kiếm
+          </button>
+        </div>
+        <div className='card-body'>
+          <div className='table-responsive'>
+            <table
+              className='table table-bordered'
+              id='dataTable'
+              width='100%'
+              cellSpacing={0}
+            >
+              <thead>
+                <tr>
+                  <th>Tên</th>
+                  <th>Họ</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>{userElements}</tbody>
+            </table>
           </div>
         </div>
+        <div className='card-footer'>
+          <Pagination
+            totalRecords={totalItems}
+            pageLimit={5}
+            pageSize={pageSize}
+            onPageChanged={onPageChanged}
+          ></Pagination>
+        </div>
       </div>
-    </Fragment>
+    </div>
+  </Fragment>
   );
 };
